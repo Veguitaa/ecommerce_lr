7d65832f8385a2437b2485e48702d2203f368e57
Veguitaa <webav2020@gmail.com>
2024-06-03 02:04:56 -0300

    slider-principal

--- js/main.js
+++ js/main.js
@@ -8,3 +8,30 @@ function open_cart() {
 function close_cart() {
     cart.classList.remove('active')
 }
+
+
+/* Initialize Swiper  */
+
+
+let swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+  /*   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }, */
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    autoplay:{
+      delay:2500,
+    }, 
+
+  });
